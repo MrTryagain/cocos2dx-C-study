db3214eb42b71987ea1824fb182be52f779745d5
MrTryAgain <z1176814811@gmail.com>
2020-07-31 15:51:04 +0800

    小球加入物理引擎，添加碰撞监听器。可以出发碰撞回调函数。

--- Classes/CrashDetection.cpp
+++ Classes/CrashDetection.cpp
@@ -1,5 +1,5 @@
 #include "CrashDetection.h"
-
+#include <iostream>
 USING_NS_CC;
 
 //extern const char* PHYSICSCONTACT_EVENT_NAME;
@@ -9,10 +9,42 @@ Scene* CrashDetection::createScene()
     //创建场景、添加层
     auto scene = Scene::createWithPhysics();
     PhysicsWorld* phyWorld = scene->getPhysicsWorld();
-    phyWorld->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+    //phyWorld->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);//debug模式，物体周围有红色线条
     auto layer = CrashDetection::create();
     scene->addChild(layer);
     return scene;
+}
+void CrashDetection::onEnter(){
+    Layer::onEnter();
+    log("onEnter");
+    
+    auto listener = EventListenerPhysicsContact::create();
+    if(listener)
+        log("创建碰撞监听器成功");
+    listener->onContactBegin = [this](PhysicsContact& contact)
+    {
+        auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
+        auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
+        a++;
+        log("碰撞了%d次",a);
+        return true;
+    };
+    
+    listener->onContactPreSolve = [] (PhysicsContact& contact,
+                                      PhysicsContactPreSolve& solve) {
+        return true;
+    };
+    
+    listener->onContactPostSolve = [] (PhysicsContact& contact,
+                                       const PhysicsContactPostSolve& solve) {
+    };
+    
+    listener->onContactSeparate = [](PhysicsContact& contact) {
+    };
+    
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithFixedPriority(
+                                                                                     listener, 1);
+    
 }
 
 // on "init" you need to initialize your instance
@@ -24,114 +56,72 @@ bool CrashDetection::init()
     {
         return false;
     }
+
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
-    PhysicsMaterial pm = {1,1,0.2};//物理属性，质量、弹性形变、摩擦力
-    //PHYSICSBODY_MATERIAL_DEFAULT//默认物理属性参数
-    //定义世界的边界
-    auto body = PhysicsBody::createEdgeBox(visibleSize,pm,5.0f);
-    auto edgeNode = Node::create();
-    edgeNode->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
-    edgeNode->setPhysicsBody(body);
-    this->addChild(edgeNode);
-
-    //设置为单点触摸
-//    auto listener = EventListenerTouchOneByOne::create();
-//    EventDispatcher* eventDispatcher = Director::getInstance()->getEventDispatcher();
-//    eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
-//    listener->onTouchBegan = CC_CALLBACK_2(CrashDetection::onTouchBeganCallback, this);
-    setTouchEnabled(true);
-    setTouchMode(Touch::DispatchMode::ONE_BY_ONE);
+    auto ball1 = Sprite::create("Ball.png");
+    ball1->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
+    PhysicsBody* ballBody1 = PhysicsBody::createCircle(ball1->getContentSize().width/2);//创建小球物理body
+    ballBody1->getShape(0)->setRestitution(1.0f);//设置球1弹性形变，getShape(0)调用形状0，可以有多个形状
+    ballBody1->getShape(0)->setFriction(0.0f);//设置球1摩擦力
+    ballBody1->getShape(0)->setDensity(1.0f);//设置球1密度
+    ballBody1->setGravityEnable(false);//设置重力
+    Vect force = Vect(500000.0f,500000.0f);
+    ballBody1->applyImpulse(force);//产生推力
+    ballBody1->setContactTestBitmask(0xFFFFFFFF);//设置能否出发碰撞监听
+    ball1->setPhysicsBody(ballBody1);//球1body添加进精灵
+    ball1->setTag(1);
+    ParticleSystem* particle1 = ParticleMeteor::create();
+    ball1->addChild(particle1);
+    this->addChild(ball1);
+    
+    auto ball2 = Sprite::create("Ball.png");
+    ball2->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
+    PhysicsBody* ballBody2 = PhysicsBody::createCircle(ball1->getContentSize().width/2);//创建小球物理body
+    ballBody2->getShape(0)->setRestitution(1.0f);//设置球1弹性形变，getShape(0)调用形状0，可以有多个形状
+    ballBody2->getShape(0)->setFriction(0.0f);//设置球1摩擦力
+    ballBody2->getShape(0)->setDensity(1.0f);//设置球1密度
+    ballBody2->setGravityEnable(false);//设置重力
+    Vect force2 = Vect(-500000.0f,-500000.0f);
+    ballBody2->applyImpulse(force2);//产生推力
+    ballBody2->setContactTestBitmask(0xFFFFFFFF);
+    ball2->setPhysicsBody(ballBody2);//球1body添加进精灵
+    ball2->setTag(1);
+    this->addChild(ball2);
+    
+    auto ball3 = Sprite::create("Ball.png");
+    ball3->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
+    PhysicsBody* ballBody3 = PhysicsBody::createCircle(ball3->getContentSize().width/2);//创建小球物理body
+    ballBody3->getShape(0)->setRestitution(1.0f);//设置球1弹性形变，getShape(0)调用形状0，可以有多个形状
+    ballBody3->getShape(0)->setFriction(0.0f);//设置球1摩擦力
+    ballBody3->getShape(0)->setDensity(1.0f);//设置球1密度
+    ballBody3->setGravityEnable(false);//设置重力
+    Vect force3 = Vect(500000.0f,500000.0f);
+    ballBody3->applyImpulse(force3);//产生推力
+    ballBody3->setContactTestBitmask(0xFFFFFFFF);
+    ball3->setPhysicsBody(ballBody3);//球1body添加进精灵
+    ball3->setTag(1);
+    this->addChild(ball3);
+    
+    Sprite* edgeSpace=Sprite::create();//创建边界精灵
+    //创建边界物理body
+    PhysicsBody* boundBody=PhysicsBody::createEdgeBox(
+                                                      visibleSize,
+                                                      PHYSICSBODY_MATERIAL_DEFAULT,
+                                                      3);
+    boundBody->getShape(0)->setFriction(0.0f);  
+    boundBody->getShape(0)->setRestitution(1.0f);
+    
+    edgeSpace->setPhysicsBody(boundBody);//将边界物理body添加进精灵
+    edgeSpace->setPosition(Point(visibleSize.width/2,visibleSize.height/2));
+    this->addChild(edgeSpace);
+    edgeSpace->setTag(0);
     return true;
 }
-
-//层进入
-void CrashDetection::onEnter()
-{
-    Layer::onEnter();
-    log("enter is ok");
-    //注册监听器，响应碰撞检测事件
-    auto listener = EventListenerPhysicsContact::create();
-    log("listener is ok");
-    listener->onContactBegin = [](PhysicsContact& contact)
-    {
-        //从接触点中取出互相接触的两个节点对象
-        auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
-        auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
-        
-        if (spriteA && spriteA->getTag() == 1
-            && spriteB && spriteB->getTag() == 1)
-        {
-            spriteA->setColor(Color3B::YELLOW);
-            spriteB->setColor(Color3B::YELLOW);
-        }
-        
-        log("onContactBegin");
-        return true;
-    };
-    
-    listener->onContactPreSolve = [] (PhysicsContact& contact, PhysicsContactPreSolve& solve)
-    {
-        log("onContactPreSolve");
-        return true;
-    };
-    
-    listener->onContactPostSolve = [] (PhysicsContact& contact, const PhysicsContactPostSolve& solve)
-    {
-        log("onContactPostSolve");
-    };
-    
-    listener->onContactSeparate = [](PhysicsContact& contact)
-    {
-        auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
-        auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
-        
-        if (spriteA && spriteA->getTag() == 1
-            && spriteB && spriteB->getTag() == 1)
-        {
-            spriteA->setColor(Color3B::WHITE);
-            spriteB->setColor(Color3B::WHITE);
-        }
-        log("onContactSeperate");
-    };
-    
-    //设置事件优先级，值越小优先级越高
-    Director::getInstance()->getEventDispatcher()->addEventListenerWithFixedPriority(listener,1);
-    
-    
-}
-//层退出
-void CrashDetection::onExit()
-{
+void CrashDetection::onExit(){
     Layer::onExit();
-    log("CrashDetection onExit");
-    //注销所有监听器
     Director::getInstance()->getEventDispatcher()->removeAllEventListeners();
+    log("移除所有监听器");
 }
-
-
-bool CrashDetection::onTouchBegan(Touch* touch, Event* event)
-{
-    Vec2 location = touch->getLocation();
-    addNewSpriteAtPosition(location);
-    return false;
-}
-
-
-void CrashDetection::addNewSpriteAtPosition(Vec2 p)
-{
-    auto sp = Sprite::create("Ball.png");
-    sp->setTag(1);
-    auto body = PhysicsBody::createBox(sp->getContentSize());
-    //设置物体接触时能否触发EventListenerPhysicsContact中定义的碰撞检测事件
-//    body->setContactTestBitmask(0x1);
-//    body->setCategoryBitmask(0x2);//类别掩码
-//    body->setCollisionBitmask(0x2);//碰撞掩码
-    
-    sp->setPhysicsBody(body);
-    sp->setPosition(p);
-    this->addChild(sp);
-}
-
--- Classes/CrashDetection.h
+++ Classes/CrashDetection.h
@@ -1,3 +1,4 @@
+
 #ifndef __CRASHDETECTION_SCENE_H__
 #define __CRASHDETECTION_SCENE_H__
 
@@ -6,21 +7,17 @@
 class CrashDetection : public cocos2d::Layer
 {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    int a = 0;
+    void onEnter();
+    void onExit();
+    cocos2d::Sprite* ball;
+    cocos2d::PhysicsWorld* m_world;
+    void setPhyWorld(cocos2d::PhysicsWorld* world) {m_world=world;};
     static cocos2d::Scene* createScene();
-    
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-    virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
-    virtual void onEnter();
-    virtual void onExit();
-    
-    // implement the "static create()" method manually
     CREATE_FUNC(CrashDetection);
-    
-    void addNewSpriteAtPosition(cocos2d::Point p);
-    
 };
 
 #endif // __CRASHDETECTION_SCENE_H__
+
 
