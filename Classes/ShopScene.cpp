2adead849051e616e8ad35a055e2545e5e8a680f
MrTryAgain <z1176814811@gmail.com>
2020-08-04 17:51:34 +0800

    游戏主场景

--- Classes/ShopScene.cpp
+++ Classes/ShopScene.cpp
@@ -18,13 +18,16 @@ bool ShopScene::init(){
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     bg->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
-    this->addChild(bg);
-    this->addChild(shopBoard,2);
-    this->addChild(shopBoard2,2);
-    this->addChild(shopBoard3,2);
-    shopBoard->setPosition(Vec2(visibleSize.width/2-100,visibleSize.height/2));
-    shopBoard2->setPosition(Vec2(visibleSize.width/2+100,visibleSize.height/2));
-    shopBoard3->setPosition(Vec2(visibleSize.width/2+300,visibleSize.height/2));
+    
+    this->addChild(bgLayer);
+    this->addChild(shopLayer);
+    bgLayer->addChild(bg);//背景是添加在bgLayer中
+    shopLayer->addChild(shopBoard,2);//商品面板添加在shopLayer中
+    shopLayer->addChild(shopBoard2,2);
+    shopLayer->addChild(shopBoard3,2);
+    shopBoard->setPosition(Vec2(shopLayer->getContentSize().width/2-100,shopLayer->getContentSize().height/2));
+    shopBoard2->setPosition(Vec2(shopLayer->getContentSize().width/2+100,shopLayer->getContentSize().height/2));
+    shopBoard3->setPosition(Vec2(shopLayer->getContentSize().width/2+300,shopLayer->getContentSize().height/2));
     shopBoard->addChild(BuyButton);
     shopBoard2->addChild(BuyButton2);
     shopBoard3->addChild(BuyButton3);
@@ -34,27 +37,43 @@ bool ShopScene::init(){
     shopBoard->setVisible(false);
     shopBoard2->setVisible(false);
     shopBoard3->setVisible(false);
+
+    auto openMenuButtonOn  = MenuItemImage::create(
+                                                   "open_menu.png",
+                                                   "open_menu.png");
+    auto openMenuButtonOff  = MenuItemImage::create(
+                                                    "close_menu.png",
+                                                    "close_menu.png");
+    auto openMenuButton = MenuItemToggle::createWithCallback(CC_CALLBACK_1(ShopScene::openMenuButtonCallback, this),
+                                                             openMenuButtonOn,
+                                                             openMenuButtonOff,
+                                                             NULL );
+    
     auto listener = EventListenerTouchOneByOne::create();//设置单点监听器
-    listener->setSwallowTouches(true);//触摸重叠层时不冲突
+    listener->setSwallowTouches(true);//触摸吞噬????????????????????????????????????
     listener->onTouchBegan = CC_CALLBACK_2(ShopScene::onTouchBegan, this);
     listener->onTouchMoved = CC_CALLBACK_2(ShopScene::onTouchMoved, this);
     listener->onTouchEnded = CC_CALLBACK_2(ShopScene::onTouchEnded, this);
     EventDispatcher* eventDispatcher = Director::getInstance()->getEventDispatcher();
     eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 
-    shopButton->setPosition(Director::getInstance()->convertToGL(Vec2(480,500)));
-    shopButton->setScale(0.5);
+    shopButton->setPosition(Vec2(850,80));
+    shopButton->setVisible(false);
+    signInButton->setPosition(Vec2(850,80));
+    signInButton->setVisible(false);
     cancelButton->setPosition(Director::getInstance()->convertToGL(Vec2(750,100)));
+    openMenuButton->setPosition(Director::getInstance()->convertToGL(Vec2(850,580)));
     cancelButton->setVisible(false);
-    auto mu = Menu::create(shopButton,cancelButton,NULL);
+    returnButton->setPosition(visibleSize.width/2,visibleSize.height/7);
+    auto mu = Menu::create(shopButton,cancelButton,returnButton,signInButton,openMenuButton,NULL);
     mu->setPosition(Vec2::ZERO);
-    this->addChild(mu);
+    bgLayer->addChild(mu);
 
     return true;
 }
 
 bool ShopScene::onTouchBegan(Touch* touch, Event* event){
-    log("开始");
+    log("点击");
     touchPoint = touch->getLocation();
     return true;
 }
@@ -83,19 +102,55 @@ void ShopScene::onTouchEnded(Touch* touch, Event* event){
         shopBoard3->runAction(MoveTo::create(0.3, board3));
     }
 }
-
+//打开商店回调
 void ShopScene::shopButtonCallback(Ref* pSender){
-    log("按钮");
+    log("打开商店");
     cancelButton->setVisible(true);
+    shopLayer->setVisible(true);
     shopBoard->setVisible(true);
     shopBoard2->setVisible(true);
     shopBoard3->setVisible(true);
 }
-
+//关闭商店按钮回调
 void ShopScene::cancelButtonCallback(Ref* pSender){
+    log("关闭商店");
     cancelButton->setVisible(false);
     shopBoard->setVisible(false);
     shopBoard2->setVisible(false);
     shopBoard3->setVisible(false);
     
 }
+//返回按钮回调
+void ShopScene::returnButtonCallback(Ref* pSender){
+    log("返回");
+    Director::getInstance()->popScene();
+}
+//签到按钮回调
+void ShopScene::signInButtonCallback(Ref* pSender){
+    log("签到");
+}
+//菜单开关按钮回调
+void ShopScene::openMenuButtonCallback(Ref* pSender){
+    log("move前商店的y坐标：%f",shopButton->getPosition().y);
+    auto item = (MenuItemToggle*)pSender;
+    if(1==item->getSelectedIndex()){//点击加号，展开菜单
+        //log("打开菜单");
+        shopButton->setVisible(true);
+        shopButton->runAction(MoveTo::create(0.3, Vec2(850,300)));
+        log("move时商店的y坐标：%f",shopButton->getPosition().y);
+        signInButton->setVisible(true);
+        signInButton->runAction(MoveTo::create(0.3, Vec2(850,200)));
+    }
+    if(0==item->getSelectedIndex()){//点击减号，收起菜单
+        log("收起菜单");
+        shopButton->runAction(MoveTo::create(0.3, Vec2(850,80)));
+        signInButton->runAction(MoveTo::create(0.3, Vec2(850,80)));
+        if(shopButton->getPosition().y<=90){
+            shopButton->setVisible(false);
+        }
+        if(signInButton->getPosition().y<=90){
+            signInButton->setVisible(false);
+        }
+    }
+}
+
--- Classes/ShopScene.h
+++ Classes/ShopScene.h
@@ -11,21 +11,37 @@ public:
     void onTouchEnded(cocos2d::Touch* touch,cocos2d::Event* event);
     void shopButtonCallback(Ref* pSender);
     void cancelButtonCallback(Ref* pSender);
-    cocos2d::Sprite* bg = cocos2d::Sprite::create("background.png");
-    cocos2d::Sprite* shopBoard = cocos2d::Sprite::create("board.png");
+    void returnButtonCallback(Ref* pSender);
+    void signInButtonCallback(Ref* pSender);
+    void dailyButtonCallback(Ref* pSender);
+    void openMenuButtonCallback(Ref* pSender);
+    cocos2d::Layer* bgLayer = Layer::create();
+    cocos2d::Layer* shopLayer = Layer::create();
+    cocos2d::Sprite* bg = cocos2d::Sprite::create("fallbg.jpg");//背景精灵
+    cocos2d::Sprite* shopBoard = cocos2d::Sprite::create("board.png");//商店面板精灵
         cocos2d::Sprite* shopBoard2 = cocos2d::Sprite::create("board.png");
         cocos2d::Sprite* shopBoard3 = cocos2d::Sprite::create("board.png");
-    cocos2d::Sprite* BuyButton = cocos2d::Sprite::create("BuyButton.png");
+    cocos2d::Sprite* BuyButton = cocos2d::Sprite::create("BuyButton.png");//确认购买精灵
         cocos2d::Sprite* BuyButton2 = cocos2d::Sprite::create("BuyButton.png");
         cocos2d::Sprite* BuyButton3 = cocos2d::Sprite::create("BuyButton.png");
-    cocos2d::MenuItemImage* shopButton = cocos2d::MenuItemImage::create(
-                                                      "go-up.png",
-                                                      "go-down.png",
+    cocos2d::MenuItemImage* shopButton = cocos2d::MenuItemImage::create(//菜单按钮-打开商店
+                                                      "shop.png",
+                                                      "shop.png",
                                                       CC_CALLBACK_1(ShopScene::shopButtonCallback, this));
-    cocos2d::MenuItemImage* cancelButton = cocos2d::MenuItemImage::create(
+    cocos2d::MenuItemImage* cancelButton = cocos2d::MenuItemImage::create(//菜单按钮-关闭商店
                                                         "Cancel.png",
                                                         "Cancel.png",
                                                         CC_CALLBACK_1(ShopScene::cancelButtonCallback, this));
+    cocos2d::MenuItemImage* returnButton = cocos2d::MenuItemImage::create(//返回按钮
+                                                                "return.png",
+                                                                "return.png",
+                                                                CC_CALLBACK_1(ShopScene::returnButtonCallback, this));
+    cocos2d::MenuItemImage* signInButton = cocos2d::MenuItemImage::create(//签到按钮
+                                                        "sign_in_up.png",
+                                                        "sign_in_down.png",
+                                                        CC_CALLBACK_1(ShopScene::signInButtonCallback, this));
+
+
     //cocos2d::Layer* shopLayer = cocos2d::Layer::create();
     cocos2d::Point touchPoint = cocos2d::Vec2(0,0);
     float moveDistance = 0;
